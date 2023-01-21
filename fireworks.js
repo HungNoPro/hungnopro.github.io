77dd842edad7560a4171831e8c19b82545e8e23b
Nguyễn Hùng <87181735+HungNoPro@users.noreply.github.com>
2023-01-21 23:43:04 +0700

    Create fireworks.js

---
+++ fireworks.js
@@ -0,0 +1,57 @@
+// Unique shell types
+const crysanthemumShell = (size=1) => {
+	const glitter = Math.random() < 0.25;
+	const singleColor = Math.random() < 0.72;
+	const color = singleColor ? randomColor({ limitWhite: true }) : [randomColor(), randomColor({ notSame: true })];
+	const pistil = singleColor && Math.random() < 0.42;
+	const pistilColor = pistil && makePistilColor(color);
+	const secondColor = singleColor && (Math.random() < 0.2 || color === COLOR.White) ? pistilColor || randomColor({ notColor: color, limitWhite: true }) : null;
+	const streamers = !pistil && color !== COLOR.White && Math.random() < 0.42;
+	let starDensity = glitter ? 1.1 : 1.25;
+	if (isLowQuality) starDensity *= 0.8;
+	if (isHighQuality) starDensity = 1.2;
+	return {
+		shellSize: size,
+		spreadSize: 300 + size * 100,
+		starLife: 900 + size * 200,
+		starDensity,
+		color,
+		secondColor,
+		glitter: glitter ? 'light' : '',
+		glitterColor: whiteOrGold(),
+		pistil,
+		pistilColor,
+		streamers
+	};
+};
+
+
+const ghostShell = (size=1) => {
+	// Extend crysanthemum shell
+	const shell = crysanthemumShell(size);
+	// Ghost effect can be fast, so extend star life
+	shell.starLife *= 1.5;
+	// Ensure we always have a single color other than white
+	let ghostColor = randomColor({ notColor: COLOR.White });
+	// Always use streamers, and sometimes a pistil
+	shell.streamers = true;
+	const pistil = Math.random() < 0.42;
+	const pistilColor = pistil && makePistilColor(ghostColor);
+	// Ghost effect - transition from invisible to chosen color
+	shell.color = INVISIBLE;
+	shell.secondColor = ghostColor;
+	// We don't want glitter to be spewed by invisible stars, and we don't currently
+	// have a way to transition glitter state. So we'll disable it.
+	shell.glitter = '';
+	
+	return shell;
+};
+
+
+const strobeShell = (size=1) => {
+	const color = randomColor({ limitWhite: true });
+	return {
+		shellSize: size,
+		spreadSize: 280 + size * 92,
+		starLife: 1100 + size * 200,
+		starLifeVariation: 0.40,
