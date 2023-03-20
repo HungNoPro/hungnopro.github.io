992cbddc15db4d2220abfe5cde512229ecd09b78
Nguyễn Hùng <87181735+HungNoPro@users.noreply.github.com>
2023-03-20 17:43:40 +0700

    Correct time setting

--- script/countdown.js
+++ script/countdown.js
@@ -1,6 +1,6 @@
 const countdown = () => {
   // Specify the date and time we are counting down to.
-  const countDate = new Date("Jun 28, 2023 00:00:00").getTime();
+  const countDate = new Date("Jun 27, 2023 00:00:00").getTime();
   const now = new Date().getTime();
   const remainingTime = countDate - now;
 
